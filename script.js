5beeda312927f1f5c7c33ff06f7c0d74922bb8ac
luisamar89 <75043731+luisamar89@users.noreply.github.com>
2021-03-22 21:45:46 +0000

    fix

--- script.js
+++ script.js
@@ -40,24 +40,3 @@ const marker = new google.maps.Marker({
 
 
 /*----------*/
-$(document).ready(function() {
-
-$(".filter-button").click(function() {
-    var value = $(this).attr('data-filter');
-
-    if(value == "all")
-    }
-        $(".filter").show('1000');
-    }
-    else
-    {
-        $(".filter").not('.'+value).hide('3000');
-        $(".filter").filter('.'+value).show('3000');
-    }
-});
-
-  if ($(".filter-button").removeClass("active")) {
-      $(this).removeClass("active");
-  }
-  $(this).addClass("active");
-});
